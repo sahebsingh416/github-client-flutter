85e1ab2e082cff69d27b7b32d5473ba7d89c4c6b
Saheb Singh <sahebsingh.tuteja.sa@gmail.com>
2020-01-07 00:56:50 +0530

    [feature] - Commit listing UI completed

---
+++ github_client_flutter/lib/ui/home.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.menu,color: Colors.white,),//Container(height: 2,width: 2,child: Image.asset("images/github.png",fit: BoxFit.fitHeight,)),
+        backgroundColor: Colors.black,
+        title: Text("GitHub"),
+        centerTitle: false,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.refresh,color: Colors.white,),
+            onPressed: (){},
+          )
+        ],
+      ),
+      body: Container(
+          height: MediaQuery.of(context).size.height,
+          child: ListView.builder(
+            itemCount: 10,
+            primary: true,
+            itemBuilder: (context, int index) {
+              return ListTile(
+                title: Text("Commits"),
+              );
+            },
+          )),
+    );
+  }
+}
